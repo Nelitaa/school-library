3209ec304e1abb039ce39f424ed8bff3e9c379fa
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-06 17:49:24 -0400

    Add public method

--- person.rb
+++ person.rb
@@ -10,8 +10,14 @@ class Person
   attr_accessor :name, :age
 
   private
-  
+
   def is_of_age?
     @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    is_of_age? || @parent_permission
   end
 end
