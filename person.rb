4ec22c8ba996bcbab9d88cef18c9e77233a919fd
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-06 17:52:15 -0400

    Fix linters

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
-class Person 
-  def initialize( age, name = "Unknown", parent_permission = true)
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -11,13 +11,13 @@ class Person
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 
   public
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 end
