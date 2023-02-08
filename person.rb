cf1d8960ef3fec347db37ff4f3d7e0a682448fb8
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-08 17:57:41 -0400

    Create therelationship between Person and Book using the intermediate class Rental

--- person.rb
+++ person.rb
@@ -1,16 +1,20 @@
 require_relative './nameable'
+require_relative './rental'
+require_relative './capitalize_decorator'
+require_relavitve './trimmer_decorator'
 
 class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
+  
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   private
 
@@ -26,5 +30,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
   end
 end
--- student.rb
+++ student.rb
@@ -1,14 +1,15 @@
 require_relative './person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_reader :classroom, :rentals
   
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    @rentals = []
   end
 
-  def play_hooky
+  def play_hsooky
     '¯(ツ)/¯'
   end
 
