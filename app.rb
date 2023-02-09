3829acb70e4a896d8917b33fe93ac970ee3f14ff
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-09 17:55:58 -0400

    Create a rental

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ require_relative 'book'
 require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
+require_relative 'rental'
 
 class App
   def initialize
     @books = []
     @people = []
+    @rentals = []
   end
 
   def list_all_books
@@ -17,7 +19,7 @@ class App
 
   def list_all_people
     @people.each do |person|
-      puts "[#{person.class}] Name: #{person.name}"
+      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
 
@@ -69,6 +71,27 @@ class App
     @books.push(book)
     puts 'Book created successfully'
   end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+    end
+    book_index = gets.chomp
+
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_index = gets.chomp
+
+    print 'Date:'
+    date = gets.chomp
+    rental = Rental.new(date, @books[book_index], @people[person_index])
+    @rentals.push(rental)
+    puts 'Rental created successfully'
+  end
+
 
 
 
