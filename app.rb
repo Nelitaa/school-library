4cdb3c050410ee2e8232a3d8ae3f6d2b35b36be1
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-09 17:33:05 -0400

    Create a person

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'book'
 require_relative 'person'
+require_relative 'student'
 
 class App
   def initialize
@@ -18,5 +19,45 @@ class App
       puts "[#{person.class}] Name: #{person.name}"
     end
   end
+
+  def create_person
+    puts "Do you want to create a student(1) or a teacher(2)? [Input the number]:"
+    person = gets.chomp
+    case person
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Invalid Option'
+    end
+  end
+
+  def create_student
+    print 'Age:'
+    age = gets.chomp
+    print 'Name:'
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]:'
+    parent_permission = gets.chomp
+    print 'Classroom:'
+    classroom = gets.chomp
+    student = Student.new(age, classroom, name, parent_permission)
+    @people.push(student)
+    puts 'Person created successfully'
+  end
+
+  def create_teacher
+    print 'Age:'
+    age = gets.chomp
+    print 'Name:'
+    name = gets.chomp
+    print 'Specialization:'
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    @people.push(teacher)
+    puts 'Person created successfully'
+  end
+
 
 
