738aeca52e70a0e3ba26ade4c96c8ee8c6334e09
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-09 16:35:32 -0400

    List all people

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require_relative 'book'
+require_relative 'person'
 
 class App
   def initialize
     @books = []
+    @people = []
   end
 
   def list_all_books
     @books.each do |book|
       puts "Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+  def list_all_people
+    @people.each do |person|
+      puts "[#{person.class}] Name: #{person.name}"
     end
   end
 
